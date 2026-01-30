952f78143e64747f22abca1a0d4cb5b7eaec43b9
Timolino28 <kordmann@web.de>
2026-01-30 07:48:04 +0100

    implemented caching for connection to database

--- db/index.ts
+++ db/index.ts
@@ -5,5 +5,19 @@ import postgres from "postgres";
 
 config({ path: ".env" });
 
-const client = postgres(process.env.DATABASE_URL!);
-export const db = drizzle({ client });
+// 1. Create a type-safe reference to the global object
+const globalForDb = globalThis as unknown as {
+    conn: postgres.Sql | undefined;
+}
+
+// 2. Reuse the existing connection if it exists, otherwise create a new one
+const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!)
+
+// 3. In development, save the connection to the global object
+// (In production, we want a fresh connection/pool every time the server starts)
+if (process.env.NODE_ENV !== "production") globalForDb.conn = conn
+
+// 4. Export the drizzle instance using the perstistent connection
+export const db = drizzle(conn)
+
+
