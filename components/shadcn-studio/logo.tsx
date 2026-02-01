ba7e2802a9d2a8f960de78f6005972a24518e3c8
Timolino28 <kordmann@web.de>
2026-02-01 14:15:52 +0100

    removed logo svg from navbar headline

--- components/shadcn-studio/logo.tsx
+++ components/shadcn-studio/logo.tsx
@@ -6,7 +6,6 @@ import { cn } from '@/lib/utils'
 const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={cn('flex items-center gap-2.5', className)}>
-      <LogoSvg className="size-8.5 " />
       <span className='text-xl font-semibold'>Philosophy</span>
     </div>
   )
