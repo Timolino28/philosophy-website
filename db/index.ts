76505004b64c7d3440eae74c49846ca5fef066e3
Timolino28 <kordmann@web.de>
2026-02-02 08:03:55 +0100

    adding handler for checking if db url is configured

--- db/index.ts
+++ db/index.ts
@@ -1,23 +1,28 @@
 import "server-only";
-import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-config({ path: ".env" });
 
 // 1. Create a type-safe reference to the global object
 const globalForDb = globalThis as unknown as {
     conn: postgres.Sql | undefined;
 }
 
-// 2. Reuse the existing connection if it exists, otherwise create a new one
-const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!)
+// 2. Check if the database URL is configured
+const databaseUrl = process.env.DATABASE_URL;
 
-// 3. In development, save the connection to the global object
+if (!databaseUrl) {
+    throw new Error("DATABASE_URL not configured");
+}
+
+// 3. Reuse the existing connection if it exists, otherwise create a new one
+const conn = globalForDb.conn ?? postgres(databaseUrl, { max: 1 })
+
+// 4. In development, save the connection to the global object
 // (In production, we want a fresh connection/pool every time the server starts)
 if (process.env.NODE_ENV !== "production") globalForDb.conn = conn
 
-// 4. Export the drizzle instance using the perstistent connection
+// 5. Export the drizzle instance using the perstistent connection
 export const db = drizzle(conn)
 
 
